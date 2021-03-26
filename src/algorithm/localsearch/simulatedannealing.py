8c62d6bdb439d5f85d1df516a3bd03c18eccfa07
a3brx <a3brx@tuta.io>
2021-03-26 22:39:46 +0000

    small fix on simulated annealing, removed dead code

--- src/algorithm/localsearch/simulatedannealing.py
+++ src/algorithm/localsearch/simulatedannealing.py
@@ -3,15 +3,3 @@ from algorithm.localsearch import LocalSearch
 class SimulatedAnnealing(LocalSearch):
     def __init__(self, model):
         super().__init__(model)
-    
-    def swap_operations(self, model):
-        pass
-
-    def insert_operation(self, model):
-        pass
-
-    def remove_operation(self, model):
-        pass
-
-    def switch_operation_drone(self, model):
-        pass
