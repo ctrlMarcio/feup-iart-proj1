1dda23559a93ed9c664397e55b33ef30d3460b21
a3brx <a3brx@tuta.io>
2021-04-03 19:48:43 +0100

    plotting function

--- new/main.py
+++ new/main.py
@@ -1,14 +1,29 @@
 from model.data import Data
 from algorithm.localsearch.hillclimbing import HillClimbing
 from algorithm.localsearch.simulatedannealing import SimulatedAnnealing
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib
 
-file = "busy_day"
-data = Data.from_input_file(file)
 
-search = HillClimbing(data, 10)
+def plot_results(graphic):
+    graphic = [[i for i, j in solution_list],
+               [j.get_score() for i, j in solution_list]]
+
+    matplotlib.rc('figure', figsize=(50, 5))
+
+    plt.plot(graphic[0], graphic[1], 'ro')
+
+    plt.show()
+
+
+data = Data.from_input_file("busy_day")
+
+search = HillClimbing(data, 50)
 # search = SimulatedAnnealing(data, 1000, 1000, 0.9)
 search.run()
 
 solution_list = search.solution_list
-print(solution_list)
+plot_results(solution_list)
 # solution.to_output_file(file)
